fb84504500b501b1d62fac1165c877c248bf9998
Fortniter90 <ethanmgenet@gmail.com>
2024-09-08 01:44:40 +1200

    now works with .env

--- my-app/src/components/FirebaseConfig.js
+++ my-app/src/components/FirebaseConfig.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDHJ2RRBJbopTuVzGgK7LzgZ84jZ2PL4uo",
-  authDomain: "codeodysseydatabase.firebaseapp.com",
-  projectId: "codeodysseydatabase",
-  storageBucket: "codeodysseydatabase.appspot.com",
-  messagingSenderId: "605483995331",
-  appId: "1:605483995331:web:212e020befc37d6c6dd276"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
