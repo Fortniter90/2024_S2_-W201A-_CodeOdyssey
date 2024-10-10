6da99da58f80f9ef185622e8f9a4f93f0d17c532
Paige Mitchell <paigemitchell323@gmail.com>
2024-10-10 23:19:32 +1300

    Updated Logged Out Homepage UI
    UI for the logged out homepage is now fully updated and should be fully dynamic for other desktop screen sizes.
    
    Only real extra things to be changed is the dropdown course menu and added extra decorations in the background. However, this is optional.

--- my-app/src/components/HomeBanner.js
+++ my-app/src/components/HomeBanner.js
@@ -2,43 +2,59 @@ import './HomeBanner.css';
 import { useEffect, useState } from 'react';
 import { FaAngleLeft, FaAngleRight } from "react-icons/fa6";
 
+// Image Imports
+import SnakeNeutral from './assets/SnakeS2-Neutral.png';
+import CapybaraSad from './assets/CapybaraS2-Sad.png';
+import BunnyConcerned from './assets/BunnyS2-Concerned.png';
+
+
+
 // HomeBanner to display courses to make users know what we have
 const HomeBanner = () => {
-    const [currentIndex, setCurrentIndex] = useState(0); // State to track the current banner index
+
+    const [currentIndex, setCurrentIndex] = useState(0); // Track the current banner index
 
     // Array of the content for the banner
     const content = [
-        {   title: 'Python', 
-            description: 'Master Python, a versatile and powerful programming language, perfect for beginners and experts alike. Dive into hands-on projects and exercises that make learning Python easy and fun.',
+        {
+            title: 'Python',
+            description: 'Unlock the power of Python, a beginner-friendly yet versatile language used in everything from web development to AI. Whether you\'re automating tasks or diving into data science, Python\'s simplicity and wide-ranging applications make it the perfect first step into programming.',
             background: 'linear-gradient(#7FBBA1, #5E9C84)',
+            image: SnakeNeutral,
         },
-        {   title: 'Java', 
-            description: 'Master Python, a versatile and powerful programming language, perfect for beginners and experts alike. Dive into hands-on projects and exercises that make learning Python easy and fun.',
+        {
+            title: 'Java',
+            description: 'Learn Java to master one of the most in-demand languages powering enterprise systems, Android apps, and large-scale web applications. Its stability, scalability, and portability make Java an essential tool for building high-performance, cross-platform solutions.',
             background: 'linear-gradient(#F69864, #F4835F)',
+            image: CapybaraSad, 
         },
-        {   title: 'C', 
-            description: 'Master Python, a versatile and powerful programming language, perfect for beginners and experts alike. Dive into hands-on projects and exercises that make learning Python easy and fun.',
+        {
+            title: 'C',
+            description: 'Get to the core of programming with C, a language that forms the backbone of modern computing. Learning C gives you a deep understanding of how software works at the hardware level, making it ideal for system programming, embedded systems, and performance-critical applications.',
             background: 'linear-gradient(#1CBCDC, #009FCC)',
+            image: BunnyConcerned,
         },
     ];
+    
+
 
     // Automatically change the banner every 5 seconds
     useEffect(() => {
         const interval = setInterval(() => {
-            setCurrentIndex(prevIndex => (prevIndex +1) % content.length);
+            setCurrentIndex(prevIndex => (prevIndex +1) % content.length);  // Cycle through banners
         }, 5000);
 
-        return () => clearInterval(interval);
-    }, []);
+        return () => clearInterval(interval); // Clear the interval when component unmounts
+    }, [currentIndex]); 
 
     // Function to go to the next banner
     const handleNext = () => {
-        setCurrentIndex((currentIndex + 1) % content.length);
+        setCurrentIndex((currentIndex + 1) % content.length);   // Increment index and loop back when reaching the end
     };
 
     // Function to go to the previous banner
     const handlePrev = () => {
-        setCurrentIndex((currentIndex - 1 + content.length) % content.length);
+        setCurrentIndex((currentIndex - 1 + content.length) % content.length);  // Decrement index and loop back when reaching the end
     };
 
     // Function to handle navigation clicks on the dots
@@ -46,15 +62,25 @@ const HomeBanner = () => {
         setCurrentIndex(index);
     };
 
+
+
     return (
         <div className='banner'>
             {/* Left arrow for the previous banner */}
             <FaAngleLeft className='arrow-left' onClick={handlePrev} />
 
+            {/* Display the image for the current banner */}
+            <img 
+                src={content[currentIndex].image} 
+                alt={`${content[currentIndex].title} banner`} 
+                className='banner-image' 
+            />
+
             {/* Banner visuals container */}
             <div className='banner-visuals' style={{ background: content[currentIndex].background }}>
-                {/* Circles decorations in banner */}
-                <svg className='circle-svg' width="100%" height="100%" viewBox="0 0 100 200" xmlns="http://www.w3.org/2000/svg">
+                
+                {/* Decorative circles in banner */}
+                <svg className='circle-svg' width="100%" height="100%" viewBox="0 0 200 200" preserveAspectRatio="xMidYMid slice"  xmlns="http://www.w3.org/2000/svg">
                     <defs>
                         {/* Drop shadow filter */}
                         <filter id="dropShadow" x="-50%" y="-50%" width="200%" height="200%">
@@ -76,19 +102,20 @@ const HomeBanner = () => {
                     </defs>
                     
                     {/* Circle one */}
-                    <circle cx="-525%" cy="225" r="150" fill="url(#circleGradient)" filter="url(#dropShadow)" />
+                    <circle cx="8%" cy="61.5%" r="14%" fill="url(#circleGradient)" filter="url(#dropShadow)" />
 
                     {/* Circle two */}
-                    <circle cx="650%" cy="175" r="250" fill="url(#circleGradient)" filter="url(#dropShadow)" />
+                    <circle cx="90%" cy="54%" r="20%" fill="url(#circleGradient)" filter="url(#dropShadow)" />
                 </svg>
 
                 {/* Banner content */}
                 <div className='banner-content'>
-                    {/* Displaying the title and description of the current banner */}
-                    <h1 className='fira-code'>Learn {content[currentIndex].title}</h1>
-                    <p className='roboto-regular'>{content[currentIndex].description}</p>
+                    <div className='banner-text'>
+                        <h1 className='fira-code'>Learn {content[currentIndex].title}</h1>
+                        <p className='roboto-regular'>{content[currentIndex].description}</p>
+                    </div>
 
-                    {/* Navigation dots for the banners */}
+                    {/* Lines for navigating between banners */}
                     <div className='banner-lines'>
                         {content.map((_, index) => (
                             <span
--- my-app/src/components/NavigationBarHome.js
+++ my-app/src/components/NavigationBarHome.js
@@ -1,31 +1,25 @@
 import "./NavigationBarHome.css";
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import { FaChevronDown } from "react-icons/fa6";
+import { FaChevronDown, FaChevronUp } from "react-icons/fa6";  // Import both icons
 import Button from "./Button";
 import { fetchCourses } from "../utils/DataFetching";
 
 const NavigationBarHome = () => {
-  const [isDropdownOpen, setDropdownOpen] = useState(false);
   const [courses, setCourses] = useState([]);
-  const [loading, setLoading] = useState(true);
   const navigate = useNavigate();
-
-  const toggleDropdown = () => {
-    setDropdownOpen(!isDropdownOpen);
-  };
 
   const goToLogin = () => {
     navigate('/login');
-  }
+  };
 
   const goToSignUp = () => {
     navigate('/signup');
-  }
+  };
 
   // Function to handle course selection and navigation
   const handleCourseSelect = (courseId) => {
-      navigate(`/course/${courseId}`);
+    navigate(`/course/${courseId}`);
   };
 
   // Load courses component on mount
@@ -33,12 +27,11 @@ const NavigationBarHome = () => {
     loadCourses();
   }, []);
 
-  // Load all of the courses
+  // Load all courses
   const loadCourses = async () => {
     try {
       const courseList = await fetchCourses();  // Fetch list of courses
-      setCourses(Object.values(courseList));    // Update the state with the fetched courses
-
+      setCourses(Object.values(courseList));    // Update the state with fetched courses
     } catch (error) {
       console.error('Error loading courses:', error); // Log any errors during data fetching
     }
@@ -46,7 +39,6 @@ const NavigationBarHome = () => {
 
   return (
     <div className="navbar roboto-regular">
-      
       <div className="navbar-left">
         <ul className="nav-links">
 
@@ -57,22 +49,21 @@ const NavigationBarHome = () => {
           </li>
 
           <li className="nav-item">
-            <span onClick={toggleDropdown}>
-              COURSES <span className="dropdown-icon"><FaChevronDown /></span>
+            <span>
+              COURSES <span className="dropdown-icon"><FaChevronDown className="chevron-down" /><FaChevronUp className="chevron-up" /></span>
             </span>
 
-            {isDropdownOpen && (
-              <ul className="dropdown-menu">
-                {courses.map(course => (
-                    <li
-                        key={course.id}
-                        onClick={() => handleCourseSelect(course.id)}
-                    >
-                        {course.title}
-                    </li>
-                ))}
-              </ul>
-            )}
+            {/* Dropdown menu */}
+            <ul className="dropdown-menu">
+              {courses.map(course => (
+                <li
+                  key={course.id}
+                  onClick={() => handleCourseSelect(course.id)}
+                >
+                  {course.title}
+                </li>
+              ))}
+            </ul>
           </li>
 
           <li className="nav-item">
@@ -93,7 +84,7 @@ const NavigationBarHome = () => {
       {/* Navigation buttons to login and sign up */}
       <div className="navbar-right">
         <Button text={"LOGIN"} outline={true} action={goToLogin} color="var(--purple-accent)" backgroundColor="var(--background-dark)" />
-        <Button text={"SIGN UP"} action={goToSignUp} color="var(--purple-accent)" backgroundColor="var(--background-dark)"/>
+        <Button text={"SIGN UP"} action={goToSignUp} color="var(--purple-accent)" backgroundColor="var(--background-dark)" />
       </div>
     </div>
   );
